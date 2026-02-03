4abf9519435f10b69f904b88e57a254d46f026ae
Przemysław_Szewczyk <56348298+Szewczor@users.noreply.github.com>
2026-02-03 14:32:00 +0100

    Add VPC and security groups for web instance

--- main.tf
+++ main.tf
@@ -14,11 +14,53 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+resource "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "web" {
+  name        = "web"
+  description = "Allow http and https. Allow everything out"
+  vpc_id      = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_role" "web_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cird_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
+}
+
+resource "aws_security_group_role" "web_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cird_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
+}
+
+resource "aws_security_group_role" "web_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cird_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   subnet_id     = "subnet-04443111f92f71936"
+
+  vpc_security_group_ids = [aws_security_group.web.id]
 
   tags = {
     Name = "HelloWorld"
